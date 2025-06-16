202bd4906929e71f83bc9df250693e9112021d8e
yamakazeAnkk <tuongankk2003@gmail.com>
2025-06-16 20:22:29 +0700

    code login and register have jwt

--- SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
+++ SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c3521ca1f7a95443084435c07c286954784fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
