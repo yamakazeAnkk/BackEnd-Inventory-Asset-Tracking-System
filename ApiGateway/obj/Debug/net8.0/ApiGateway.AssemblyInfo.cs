4b70ebc9a17c57648454600fa143e53e56fa6ff7
yamakazeAnkk <tuongankk2003@gmail.com>
2025-06-26 21:35:39 +0700

    Separation of Concerns

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202bd4906929e71f83bc9df250693e9112021d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77042cf07493d33826624423496413336d4817b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,9 +1,33 @@
+using AuthService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using AuthService.Application.Interfaces;
+using AuthService.Infrastructure.Services;
+using AuthService.Configuration;
+using AuthService.Infrastructure.Repositories.Ef;
+using AuthService.Infrastructure.Repositories.Interfaces;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+// Register JwtOptions and TokenService
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAuthService, AuthService.Infrastructure.Services.AuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
@@ -14,8 +38,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
-app.Run(); 
+app.Run();
+
